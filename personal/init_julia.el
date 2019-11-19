09d50679c7698deae899e567502fdbba07d7e0db
Boncheol Gu <boncheol.gu@gmail.com>
2019-11-19 16:03:58 +0900

    Remove julia configuration

--- personal/init_julia.el
+++
@@ -1,9 +0,0 @@
-(prelude-require-packages
- '(julia-mode julia-repl))
-
-(quelpa '(lsp-julia :fetcher github :repo "non-Jedi/lsp-julia"))
-
-(add-hook 'julia-mode-hook 'julia-repl-mode) ;; always use minor mode
-(add-hook 'julia-repl-hook #'julia-repl-use-emacsclient)
-(add-hook 'julia-repl-hook #'lsp-mode)
-
